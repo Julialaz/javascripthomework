89de3dfdf3d516912b3234d42dbf895e1c189651
Julia Lazurkevych <lazurkevich@yahoo.com>
2016-10-03 17:19:25 +0000

    add new file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// JavaScript File
+var a=Number(prompt("Please enter value a"));
+var b=Number(prompt("Please enter value b"));
+var c=Number(prompt("Please enter value c"));
+
+var x,x1,x2;
+var d=b*b-4*a*c;
+
+if (d>0) 
+{document.writeln("No roots");
+} 
+if (d===0); 
+{2*a*x+b===0; 
+x=(-b)/(2*a);
+document.writeln(x);}
+if (d>0);
+{var x1=(-b+Math.sqrt(d))/(2*a);
+var x2=(-b-Math.sqrt(d))/(2*a);
+document.writeln(x1);document.writeln(x2)}
+
+
+
+
+
