cfa5b24b641025493e31acfde10d2186451c4072
Julia Lazurkevych <lazurkevich@yahoo.com>
2016-10-04 09:57:46 +0000

    add new file

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var b=Number(prompt("Please enter value b"));
 var c=Number(prompt("Please enter value c"));
 
 var x,x1,x2;
-var d=b*b-4*a*c;
+var d=Math.pow(b,2)-4*a*c;
 
-if (d>0) 
+if (d<0) 
 {document.writeln("No roots");
 } 
 if (d===0); 
 {2*a*x+b===0; 
-x=(-b)/(2*a);
+x=-b/(2*a);
 document.writeln(x);}
 if (d>0);
 {var x1=(-b+Math.sqrt(d))/(2*a);
